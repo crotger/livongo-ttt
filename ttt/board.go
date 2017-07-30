b9b895203cad1dbc262ba0d68084874114df58ba
Carlos Rotger <carlos_rotger@brown.edu>
2017-07-30 17:31:32 -0400

    win checking mechanism + board tostring

--- ttt/board.go
+++ ttt/board.go
@@ -1,6 +1,7 @@
 package ttt
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -60,6 +61,92 @@ func (b *Board) cell2RowCol(cell int) (int, int) {
 
 func (b *Board) rowCol2Cell(row int, col int) int {
 	return row*b.Cols + col
+}
+
+type victory struct {
+	mark Marker
+
+	start int
+	end   int
+}
+
+func (b *Board) CheckForWinner(lastMoveCell int) *victory {
+	row, col := b.cell2RowCol(lastMoveCell)
+	mark := b.cells[lastMoveCell]
+
+	//vertical
+	vic := b.check(b.generateCellList(row-b.victoryNumber+1, col, 1, 0), mark)
+	if vic != nil {
+		return vic
+	}
+
+	//horizontal
+	vic = b.check(b.generateCellList(row, col-b.victoryNumber+1, 0, 1), mark)
+	if vic != nil {
+		return vic
+	}
+
+	//diagonal down
+	vic = b.check(b.generateCellList(row-b.victoryNumber+1, col-b.victoryNumber+1, 1, 1), mark)
+	if vic != nil {
+		return vic
+	}
+
+	//diagonal up
+	vic = b.check(b.generateCellList(row+b.victoryNumber-1, col-b.victoryNumber+1, -1, 1), mark)
+	return vic
+}
+
+func (b *Board) generateCellList(startRow int, startCol int, dirRow int, dirCol int) []int {
+	results := []int{}
+	for i := 0; i < b.victoryNumber*2-1; i++ {
+		r, c := startRow+(i*dirRow), startCol+(i*dirCol)
+		if r < 0 || c < 0 || r >= b.Rows || c >= b.Cols {
+			continue
+		}
+		results = append(results, b.rowCol2Cell(r, c))
+	}
+	return results
+}
+
+func (b *Board) check(locs []int, mark Marker) *victory {
+
+	numAdj := 0
+	for i, cell := range locs {
+		m := b.cells[cell]
+		if m != mark {
+			numAdj = 0
+		}
+
+		if m == mark {
+			numAdj += 1
+			if numAdj >= b.victoryNumber {
+				return &victory{
+					mark:  mark,
+					start: locs[i-b.victoryNumber+1],
+					end:   cell,
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (b Board) String() (s string) {
+	var buf bytes.Buffer
+	sep := "|"
+
+	for i, m := range b.cells {
+		fmt.Fprint(&buf, m)
+		if i%b.Cols == b.Cols-1 {
+			fmt.Fprint(&buf, "\n")
+		} else {
+			fmt.Fprint(&buf, sep)
+		}
+	}
+
+	return buf.String()
 }
 
 func (m Marker) String() (s string) {

0a63b1582a907ff90d6a40cc8a819be414ed9c18
Carlos Rotger <carlos_rotger@brown.edu>
2017-08-02 11:11:24 -0400

    main game loop

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/crotger/livongo-challenge/ttt"
+	"time"
+)
+
+func main() {
+	b := ttt.DefaultBoard()
+	fmt.Println("Welcome! What's your name?")
+	var name string
+	fmt.Print("Your name: ")
+	fmt.Scanln(&name)
+
+	p := &ttt.HumanPlayer{name, ttt.MARK_X}
+	players := []ttt.Player{&ttt.RandomAI{ttt.MARK_O}, p}
+
+	time.Sleep(500 * time.Millisecond)
+	fmt.Printf("Welcome, %v, the computer will go first.\n", p.Name())
+
+	gameOver := false
+	currentPlayer := 0
+	for !gameOver {
+		time.Sleep(500 * time.Millisecond)
+		cp := players[currentPlayer]
+		fmt.Println(cp.Name(), "'s turn.")
+		cell, err := cp.Play(b)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		time.Sleep(500 * time.Millisecond)
+		b.Set(cell, cp.Marker())
+		fmt.Println(cp.Name(), "placed an", cp.Marker(), "at", cell+1)
+		fmt.Println(b)
+
+		vic := b.CheckForWinner(cell)
+		if vic != nil {
+			gameOver = true
+			fmt.Println(cp.Name(), "wins!")
+		}
+
+		if b.IsFull() {
+			gameOver = true
+		}
+
+		currentPlayer = (currentPlayer + 1) % 2
+	}
+}
